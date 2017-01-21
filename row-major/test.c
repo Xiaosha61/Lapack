9e017c34b86d580013c2a10923ce296fe8c972d6
Xiaosha61 <xxssfamily61@gmail.com>
2017-01-21 17:32:24 +0100

    add col-major

--- row-major/test.c
+++ row-major/test.c
@@ -11,19 +11,12 @@ int main (int argc, const char * argv[])
    double B[3][1] = {1,2,3};
    lapack_int info,m,n,lda,ldb,nrhs;
    int i,j;
-/*
+
    m = 3;  // #rows of A
    n = 2;	// #cols of A
    nrhs = 1;
    lda = 2;  // #rows of coefficients
    ldb = 1;  // #cols of coefficients
-*/
-   m = 2; 
-   n = 2;	
-   nrhs = 1;
-   lda = 3;  
-   ldb = 3;  
-
 
 
    info = LAPACKE_dgels(LAPACK_ROW_MAJOR,'N',m,n,nrhs,*A,lda,*B,ldb);
