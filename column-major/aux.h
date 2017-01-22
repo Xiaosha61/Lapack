84e9579054bc11a8e6bee89629a370093daf5f1d
Xiaosha61 <xxssfamily61@gmail.com>
2017-01-22 12:40:26 +0100

    modify aux.h in column-major

--- column-major/aux.h
+++ column-major/aux.h
@@ -1,9 +1,7 @@
-#ifndef _AUX_
-#define _AUX_
-
+#include <stdio.h>
+#include <stdlib.h>
 
 void print_matrix_rowmajor( char* desc, lapack_int m, lapack_int n, double* mat, lapack_int ldm );
 void print_matrix_colmajor( char* desc, lapack_int m, lapack_int n, double* mat, lapack_int ldm );
 void print_vector( char* desc, lapack_int n, lapack_int* vec );
 
-#endif /* _AUX_*/
